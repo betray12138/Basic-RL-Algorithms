a7a7a7c0deb6a5bfd4ff32e2bef91611101e6664
betray12138 <2318206376@qq.com>
2023-09-21 17:18:40 +0800

    update td3

--- main_td3.py
+++ main_td3.py
@@ -10,7 +10,7 @@ from env.env_wrapper import GymNasiumWrapper
 from algo.td3 import TD3
 
 
-parser = argparse.ArgumentParser(description='PyTorch DDPG')
+parser = argparse.ArgumentParser(description='PyTorch TD3')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
 					help='discount factor (default: 0.99)')
 parser.add_argument('--seed', type=int, default=0, metavar='N',
